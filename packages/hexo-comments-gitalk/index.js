b9c7ffc77da728caac76eede47c5885b16a53c7c
Huazie <huazie.lgh@gmail.com>
2025-11-10 20:41:41 +0800

    Mod：取合并配置的工具类调整

--- packages/hexo-comments-gitalk/index.js
+++ packages/hexo-comments-gitalk/index.js
@@ -1,7 +1,7 @@
 'use strict';
 
-const Util = require('@next-theme/utils');
-const utils = new Util(hexo, __dirname);
+const { Utils } = require('hexo-generator-comments/lib/utils');
+const utils = new Utils(hexo, __dirname);
 
 // 在 Hexo 初始化完成后输出插件信息
 hexo.on('ready', () => {
@@ -13,7 +13,7 @@ hexo.on('ready', () => {
 // 添加 Gitalk 评论系统相关的Diversity主题注入过滤器
 hexo.extend.filter.register('theme_injects', injects => {
     // 合并优先级：站点配置 > 主题配置 > 默认配置
-    let config = utils.defaultConfigFile('gitalk', 'default.yml');
+    let config = utils.getMergedConfig('gitalk', 'default.yml');
     // 主题配置中的gitalk配置覆盖，便于评论相关的布局文件中使用
     hexo.theme.config.gitalk = config;
 
@@ -43,7 +43,7 @@ hexo.extend.filter.register('theme_injects', injects => {
         hexo.log.error('gitalk.client_secret can\'t be null.');
         return;
     }
- 
+
     // comment 视图添加 gitalk
     injects.comment.raw('gitalk', '<div class="comments gitalk-container"></div>', {}, { cache: true });
 
