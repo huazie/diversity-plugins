f0501fc8e21719163c656a6a73588eb07ce3140f
Huazie <huazie.lgh@gmail.com>
2025-11-10 20:31:18 +0800

    Mod：取合并配置的工具类调整

--- packages/hexo-comments-utterances/index.js
+++ packages/hexo-comments-utterances/index.js
@@ -1,7 +1,7 @@
 'use strict';
 
-const Util = require('@next-theme/utils');
-const utils = new Util(hexo, __dirname);
+const { Utils } = require('hexo-generator-comments/lib/utils');
+const utils = new Utils(hexo, __dirname);
 
 // 在 Hexo 初始化完成后输出插件信息
 hexo.on('ready', () => {
@@ -13,10 +13,10 @@ hexo.on('ready', () => {
 // 添加 Utterances 评论系统相关的Diversity主题注入过滤器
 hexo.extend.filter.register('theme_injects', injects => {
     // 合并优先级：站点配置 > 主题配置 > 默认配置
-    let config = utils.defaultConfigFile('utterances', 'default.yml');
+    let config = utils.getMergedConfig('utterances', 'default.yml');
     // 主题配置中的utterances配置覆盖，便于评论相关的布局文件中使用
     hexo.theme.config.utterances = config;
-    
+
     // 没有启用 Utterances
     if (!config.enable) return;
 
@@ -28,7 +28,7 @@ hexo.extend.filter.register('theme_injects', injects => {
 
     // comment 视图添加 utterances
     injects.comment.raw('utterances', '<div class="comments utterances-container"></div>', {}, { cache: true });
-    
+
     // pageEnd 视图添加 utterances
     injects.pageEnd.raw('utterances', utils.getFileContent('layout/utterances' + injects.pageEnd.extName));
 
