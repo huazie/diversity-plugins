ff3c8b15c0c88b3ed29438121bb46c4e3abfc7c9
Huazie <huazie.lgh@gmail.com>
2025-11-10 21:03:44 +0800

    Mod：取合并配置的工具类调整

--- packages/hexo-comments-giscus/index.js
+++ packages/hexo-comments-giscus/index.js
@@ -1,7 +1,7 @@
 'use strict';
 
-const Util = require('@next-theme/utils');
-const utils = new Util(hexo, __dirname);
+const { Utils } = require('hexo-generator-comments/lib/utils');
+const utils = new Utils(hexo, __dirname);
 
 // 在 Hexo 初始化完成后输出插件信息
 hexo.on('ready', () => {
@@ -13,10 +13,10 @@ hexo.on('ready', () => {
 // 添加 Giscus 评论系统相关的Diversity主题注入过滤器
 hexo.extend.filter.register('theme_injects', injects => {
     // 合并优先级：站点配置 > 主题配置 > 默认配置
-    let config = utils.defaultConfigFile('giscus', 'default.yml');
+    let config = utils.getMergedConfig('giscus', 'default.yml');
     // 主题配置中的giscus配置覆盖，便于评论相关的布局文件中使用
     hexo.theme.config.giscus = config;
-    
+
     // 没有启用 Gitalk
     if (!config.enable) return;
 
